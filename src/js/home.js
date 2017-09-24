c1623db35c791cf9dc87c99da3a0edd6e657f915
Dhara Shah <DharaShah@Dharas-MacBook-Air.local>
2017-09-24 10:41:04 +0530

    added incident cards in middle section

--- src/js/home.js
+++ src/js/home.js
@@ -13,6 +13,67 @@ function getJSON(url, callback) {
   xhr.send();
 };
 
+//Middle column
+getJSON('https://cdn.protograph.pykih.com/be0b3c8854f0b1e774b96580/index.json', function (err, data){
+  if (err != null) {
+    alert('Something went wrong: ' + err);
+  } else {      
+    let mob_cards = '';
+    data.map((d,i) => {
+      let img = d.screen_shot_url,
+        new_date = d.date.split("-"),
+        month = new Date(d.date).toLocaleDateString('en-US', {month: 'long'});
+      mob_cards += '<div id="ProtoCard-'+ i +'" class="mob-justice-incidents" style="height:220px;overflow:hidden;">' +
+        '<img src="'+img+ '" width="100%"/>'+
+        '<div class="protograph-gradient">'+
+          '<div class="data-card-content">'+
+            '<div class="data-card-title">' + d.title + '</div>'+
+            '<div class="data-card-date">' + new_date[2]+"th "+ month + " "+ new_date[0]+'</div>' +
+            '</div>'+
+          '</div>'+
+        '</div>'
+      document.getElementById('display-cards').innerHTML = mob_cards
+    })
+    let dimension = getScreenSize(), mode;
+    if (dimension.width <= 400){
+      mode = 'mobile';
+    } else {
+      mode = 'laptop';
+    } 
+    for (let i=0; i<data.length; i++) {
+      let createDiv = document.createElement('div');
+        createDiv.id = 'ProtoCard-'+i
+        document.getElementById('ProtoCard-'+i).setAttribute("data-toggle", "modal")
+        document.getElementById('ProtoCard-'+i).setAttribute("data-target", "#proto-modal")
+      document.getElementById('ProtoCard-'+i).addEventListener('click', function (d) {
+        $('#proto-modal').modal('show');
+        // $('#proto-modal').on('hidden.bs.modal', function(){
+        //   let element = document.querySelector("#proto-embed-card iframe");
+        //   element.parentNode.removeChild(element);
+        // })
+        // modal({
+        //   onShow: function() {
+        //     $("#proto-modal").css("height", 0)
+        //   },
+        //   onHide: function(){
+        //     if (mode === 'laptop'){
+        //       $("#proto-modal").css("height", "100%")
+        //     }
+        //   },
+        //   onHidden: function(e) {
+            
+        //   }
+        // }).modal('attach events', '.close').modal('show')
+        // if (mode === 'laptop') {
+        //   let pro = new ProtoEmbed.initFrame(document.getElementById("proto-embed-card"), data[i].iframe_url, 'laptop')
+        // } else {
+        //   let pro = new ProtoEmbed.initFrame(document.getElementById("proto-embed-card"), data[i].iframe_url, 'mobile', true)
+        // } 
+      })
+    }
+  }
+})
+
 //twitter chatter
 getJSON('https://protograph.newslaundry.com/toReportJournalistKilling/twitter.json', function (err, data){
     if (err != null) {
@@ -34,35 +95,38 @@ getJSON('https://protograph.newslaundry.com/toReportJournalistKilling/twitter.js
     }
   })
 
-// <div class="proto-card tolink-card">
-//             <div class="briefs-layout">
-//               <div class="card-text">As Islamic State targets Southeast Asia as its next theater of destruction, regional authorities are grappling with how to stem its underground cash flows. </div>
-//               <div class="by-time-line">
-//                 <div class="by-line">Alan Byod</div>
-//               </div>
-//               <div class="hint-text">10 news stories talking about this</div>
-//             </div>
-//           </div>
+let interval = setInterval(function(){   
+  getJSON('https://protograph.newslaundry.com/toReportJournalistKilling/twitter.json', function (err, data){
+    if (err != null) {
+      alert('Something went wrong: ' + err);
+    } else {
+      let tweets = '';
+      data.map((d,i) => {       
+        tweets += '<a href="'+d.canonical+'" target="_blank" class="protograph-url"><div class="proto-card tolink-card">'+
+          '<div class="briefs-layout">'+
+            '<div class="card-text">' + d.description + '</div>'+
+            '<div class="by-time-line">'+
+              '<div class="by-line">' + d.author + '</div>'+
+            '</div>'+
+            '<div class="hint-text">'+ d.date +'</div>'+
+          '</div>'+
+        '</div></a>';
+      document.getElementById('display-tweets').innerHTML = tweets;   
+      })
+    }
+  })
+}, 60000)
 
-// let interval = setInterval(function(){   
-//   getJSON('https://cdn.protograph.pykih.com/toReportViolence/twitter.json', function (err, data){
-//     if (err != null) {
-//       alert('Something went wrong: ' + err);
-//     } else {
-//       let tweets = '';
-//       data.map((d,i) => {       
-//         tweets += '<div style="margin-bottom:20px;padding:5px;"><a href="'+d.canonical+'" target="_blank" class="protograph-url">' +
-//           '<div style="margin-bottom:10px">'+
-//             '<img class="tolink-profile-image" src="'+d.author_image+'">'+
-//             '<div class="profile-title-div">'+
-//               '<div style="margin-bottom:0px">'+d.author+'</div>'+
-//               '<div class="tolink-light-text">@' +d.twitter_handle+'</div>'+
-//             '</div>'+
-//           '</div>'+         
-//           '<p>'+d.description+'</p>'+
-//         '</a></div>' 
-//       document.getElementById('display-tweets').innerHTML = tweets;   
-//       })
-//     }
-//   })
-// }, 60000)
+function getScreenSize() {
+  let w = window,
+    d = document,
+    e = d.documentElement,
+    g = d.getElementsByTagName('body')[0],
+    width = w.innerWidth || e.clientWidth || g.clientWidth,
+    height = w.innerHeight|| e.clientHeight|| g.clientHeight;
+
+  return {
+    width: width,
+    height: height
+  };
+}
