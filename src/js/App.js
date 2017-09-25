92b1a08c5de2239ee9ea32944319dba6d2bb02dc
Dhara Shah <DharaShah@Dharas-MacBook-Air.local>
2017-09-25 19:30:00 +0530

    changed the summary line

--- src/js/App.js
+++ src/js/App.js
@@ -410,7 +410,7 @@ class App extends React.Component {
                   <span className="animate-number">{number_of_incidents}</span>
                 </div>
               </div>
-              <div className="display-text">Instances of journalist killings were reported
+              <div className="display-text">Instances of lynchings and mob vigilantism were reported 
                {start_date === '' || end_date === '' ? '' : ` from ${start_date} to ${end_date}` }
               </div>
               <TimeBrush dataJSON={this.state.filteredJSON} dimensionWidth={this.props.dimensionWidth} start_domain={this.state.start_domain} end_domain={this.state.end_domain} mode={this.props.mode} handleSelectDateRange={this.handleSelectDateRange}/>
