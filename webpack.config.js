bedd77893b76c5dd7f99fe2b4e98c1f31657245e
Dhara Shah <DharaShah@Dharas-MacBook-Air.local>
2017-09-27 14:33:52 +0530

    minifying and creating files for production

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,9 @@ const webpack = require('webpack');
 const card = require('./webpack.config.card.js');
 const about = require('./webpack.config.about.js');
 const home = require('./webpack.config.home.js');
+const hate = require('./webpack.config.hate-crime.js');
 
 
 module.exports = [
-  card, about, home
+  card, about, home, hate
 ];
