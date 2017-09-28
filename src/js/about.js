63276cf41776d5a8915a0372d7cb8e914a8eca84
AashutoshSBhatt <aashutosh.bhatt@pykih.com>
2017-09-28 20:28:36 +0530

    Adds scrollspy in about and findings page.

--- src/js/about.js
+++ src/js/about.js
@@ -2,17 +2,13 @@ let dimension = getScreenSize();
 $('.explainers-sticky-segment').sticky({topSpacing:20});
 $('.social-share-icons').sticky({topSpacing: dimension.height - 100})
 $('.filter-column').sticky({topSpacing:20});
-$(window).scroll(function(event){
-  $('.qa-highlight-color').removeClass('qa-highlight-color');
-  $('.band_color').removeClass('band_color');
-  var cutoff = $(window).scrollTop();
-  $('.answer').addClass('dull-out');
-  $('.answer').removeClass('qa-highlight-color').each(function() {
-    if ($(this).offset().top >= cutoff) {
-      $(this).removeClass('dull-out');
-      $('#q'+$(this).attr('id')).addClass('qa-highlight-color');
-      $('#q'+$(this).attr('id')).addClass('band_color');
-      return false; // stops the iteration after the first one on screen
+$(document).ready(function () {
+  $(window).on("activate.bs.scrollspy", function(e){
+    $('.answer').addClass('dull-out');
+    var current = $('#myNavbar li a.active');
+    if(current.length) {
+      var target_element = current.get(0).hash.split('_')[0];
+      $(target_element).removeClass('dull-out');
     }
   });
 });
