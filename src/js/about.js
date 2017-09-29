6e87c29eb5fe8d02f7cf591971c19e2029b4d298
AashutoshSBhatt <aashutosh.bhatt@pykih.com>
2017-09-29 14:17:13 +0530

    Adds submenu in findings page, also highlights the first tab on page.

--- src/js/about.js
+++ src/js/about.js
@@ -5,13 +5,41 @@ $('.filter-column').sticky({topSpacing:20});
 $(document).ready(function () {
   $(window).on("activate.bs.scrollspy", function(e){
     $('.answer').addClass('dull-out');
-    var current = $('#myNavbar li a.active');
-    if(current.length) {
-      var target_element = current.get(0).hash.split('_')[0];
+    var current = $('#myNavbar li a.active'),
+      target_element,
+      target_element_parent;
+
+    switch(current.length) {
+      case 0:
+        return;
+      case 2:
+        target_element = current.get(1).hash;
+        break;
+      case 1:
+        target_element = current.get(0).hash;
+        break;
+    }
+
+    if(target_element) {
+      target_element_parent = target_element.split('_');
+      if (!(target_element_parent.length > 1)) {
+        $(target_element).find('.answer').removeClass('dull-out');
+      } else {
+        $(target_element_parent[0]).removeClass('dull-out');
+      }
       $(target_element).removeClass('dull-out');
     }
   });
 });
+
+$(window).scroll(function () {
+  var active_ancors = $('#myNavbar li a.active'),
+    active_ancors_length = active_ancors.length;
+  if (!active_ancors_length) {
+    $('#myNavbar li a:first').addClass('active');
+  }
+
+})
 
 $('.question').hover(function(){
   $(this).addClass('qa-highlight-color');
@@ -20,11 +48,11 @@ $('.question').hover(function(){
   $(this).removeClass('qa-highlight-color');
   $( $(this).children('div').children('a')[0].hash ).removeClass('qa-highlight-color');
 });
-$('.answer').hover(function(){
-  $(this).addClass('qa-highlight-color');
-},function(){
-  $(this).removeClass('qa-highlight-color');
-})
+// $('.answer').hover(function(){
+//   $(this).addClass('qa-highlight-color');
+// },function(){
+//   $(this).removeClass('qa-highlight-color');
+// })
 
 function getScreenSize() {
   let w = window,
